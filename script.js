a935033ae30efdc98afd4c189f1dc2a2bae7eb9d
andreymaks <andreymaks39@gmail.com>
2024-06-03 15:21:41 +0200

    PacMan movement

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const field = document.querySelector(".field");
 const boardSize = 20;
+const pacman = { x: 1, y: 1 };
+let direction = { x: 0, y: 0 };
+let canMove = true;
 const map = [
   "wwwwwwwwwwwwwwwwwwww",
   "w....wwwwwwwwww....w",
@@ -23,11 +26,14 @@ const map = [
   "wwwwwwwwwwwwwwwwwwww",
 ];
 
+const calculateIndex = function () {
+  return (index = pacman.x * boardSize + pacman.y);
+};
+
 const drawMap = function () {
   map.forEach((row, i) => {
     [...row].forEach((cell, j) => {
       const el = document.createElement("div");
-      const index = i * boardSize + j;
       const cellClass = cell === "w" ? "wall" : "food";
       el.classList.add(`${cellClass}`);
       field.appendChild(el);
@@ -36,3 +42,53 @@ const drawMap = function () {
 };
 
 drawMap();
+
+const drawPacMan = function () {
+  const index = calculateIndex();
+  field.children[index].classList.remove("food");
+  field.children[index].classList.add("pacman");
+};
+
+drawPacMan();
+
+const movePacMan = function () {
+  if (map[pacman.x + direction.x][pacman.y + direction.y] !== "w") {
+    const index = calculateIndex();
+    field.children[index].classList.remove("pacman");
+    pacman.x += direction.x;
+    pacman.y += direction.y;
+    drawPacMan();
+  }
+};
+
+const keyDown = function (e) {};
+
+document.addEventListener("keydown", function (e) {
+  e.preventDefault();
+  if (canMove) {
+    canMove = false;
+    setTimeout(function () {
+      canMove = true;
+    }, 150);
+    switch (e.key) {
+      case "ArrowUp":
+        direction = { x: -1, y: 0 };
+        movePacMan();
+        break;
+      case "ArrowDown":
+        direction = { x: 1, y: 0 };
+        movePacMan();
+        break;
+      case "ArrowLeft":
+        direction = { x: 0, y: -1 };
+        movePacMan();
+        break;
+      case "ArrowRight":
+        direction = { x: 0, y: 1 };
+        movePacMan();
+        break;
+      default:
+        break;
+    }
+  }
+});
